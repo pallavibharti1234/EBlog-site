2de49430ef50f0c2bc5c4f38c18c9cb3b66a5073
pallavibharti1234 <57215276+pallavibharti1234@users.noreply.github.com>
2020-12-21 13:05:53 +0530

    Add files via upload

--- header.php
+++ header.php
@@ -6,183 +6,101 @@
  * (hillsofts@gmail.com)
  * ................................................................
  */
-include "../connect.php";
-session_start();
-if(!isset($_SESSION['SESS_FIRST_NAME'])){
-    header("location: sign-in.php");
-}
+include "connect.php";
 ?>
 <!DOCTYPE HTML>
-<html>
+<html class="no-js">
+<!-- Mirrored from html.imithemes.com/nativechurch/ by HTTrack Website Copier/3.x [XR&CO'2014], Wed, 09 Nov 2016 06:05:14 GMT -->
+<!-- Added by HTTrack --><meta http-equiv="content-type" content="text/html;charset=UTF-8" /><!-- /Added by HTTrack -->
 <head>
-<?php
-                           // include('../connect.php');
-				$result = $db->prepare("SELECT * FROM settings");
-				$result->execute();
-				for($i=0; $row = $result->fetch(); $i++){
-
-
-               ?>
-<title><?php  echo $row['site_title']?></title>
-<?php } ?>
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<meta name="keywords" content="Easy Admin Panel Responsive web template, Bootstrap Web Templates, Flat Web Templates, Android Compatible web template, 
-Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, SonyEricsson, Motorola web design" />
-<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
- <!-- Bootstrap Core CSS -->
-<link href="css/bootstrap.min.css" rel='stylesheet' type='text/css' />
-<!-- Custom CSS -->
-<link href="css/style.css" rel='stylesheet' type='text/css' />
-<!-- Graph CSS -->
-<link href="css/font-awesome.css" rel="stylesheet"> 
-<!-- jQuery -->
-<!-- lined-icons -->
-<link rel="stylesheet" href="css/icon-font.min.css" type='text/css' />
-<!-- //lined-icons -->
-<!-- chart -->
-<script src="js/Chart.js"></script>
-<!-- //chart -->
-<!--animate-->
-<link href="css/animate.css" rel="stylesheet" type="text/css" media="all">
-<script src="js/wow.min.js"></script>
-	<script>
-		 new WOW().init();
-	</script>
-<!--//end-animate-->
-<!----webfonts--->
-<link href='//fonts.googleapis.com/css?family=Cabin:400,400italic,500,500italic,600,600italic,700,700italic' rel='stylesheet' type='text/css'>
-<!---//webfonts---> 
- <!-- Meters graphs -->
-<script src="js/jquery-1.10.2.min.js"></script>
-<!-- Placed js at the end of the document so the pages load faster -->
-<script src="//cdn.ckeditor.com/4.5.7/standard/ckeditor.js"></script>
-<script src="../admin/ckeditor/ckeditor.js"></script>
-
-</head> 
-   
- <body class="sticky-header left-side-collapsed"  onload="initMap()">
-    <section>
-    <!-- left side start-->
-		<div class="left-side sticky-left-side">
-
-			<!--logo and iconic logo start-->
-			<div class="logo">
-			<?php
-                           // include('../connect.php');
-				$result = $db->prepare("SELECT * FROM settings");
-				$result->execute();
-				for($i=0; $row = $result->fetch(); $i++){
-
-
-               ?>
-				<h1><a href="index.php">Eblog </a></h1>
-			<?php }?>
-			</div>
-			<div class="logo-icon text-center">
-				<a href="index.php"><i class="lnr lnr-home"></i> </a>
-			</div>
-
-			<!--logo and iconic logo end-->
-			<div class="left-side-inner">
-
-				<!--sidebar nav start-->
-					<ul class="nav nav-pills nav-stacked custom-nav">
-						<li class=""><a href="index.php"><i class="lnr lnr-power-switch"></i><span>Dashboard</span></a></li>
-						<li class="menu-list">
-							<a href="#"><i class="lnr lnr-pencil"></i>
-								<span>Manage News</span></a>
-								<ul class="sub-menu-list">
-									<li><a href="compose-news.php">Compose News</a> </li>
-									<li><a href="all-news.php">All News</a></li>
-								</ul>
-						</li>
-						<li class="menu-list">
-							<a href="#"><i class="fa fa-user"></i>
-								<span>Manage Excos</span></a>
-								<ul class="sub-menu-list">
-									<li><a href="add-exco.php">Add New</a> </li>
-									<li><a href="all-excos.php">All Excos</a></li>
-								</ul>
-						</li>
-						<li class="menu-list"><a href="#"><i class="fa fa-globe"></i> <span>Manage Events</span></a>
-							<ul class="sub-menu-list">
-								<li><a href="add-event.php">New Event</a> </li>
-								<li><a href="all-events.php">All Events</a></li>
-							</ul>
-						</li> 
-						<li class="menu-list"><a href="#"><i class="fa fa-instagram"></i> <span>Manage Gallery</span></a>
-							<ul class="sub-menu-list">
-								<li><a href="add-photo.php">New Photo</a> </li>
-								<li><a href="all-photos.php">All Photos</a></li>
-							</ul>
-						</li> 
-                                                <li class="menu-list"><a href="#"><i class="fa fa-camera"></i> <span>Manage Slider</span></a>
-							<ul class="sub-menu-list">
-								<li><a href="add-slider.php">New Photo</a> </li>
-								<li><a href="all-slider.php">All Photos</a></li>
-							</ul>
-						</li> 					
-						<li class="menu-list">
-							<a href="#"><i class="lnr lnr-cog"></i>
-								<span>Manage Site</span></a>
-								<ul class="sub-menu-list">
-								<li><a href="add-welcome.php">Welcome Note</a> </li>
-									<li><a href="add-about.php">About Us</a> </li>
-									<li><a href="add-program.php">Our Programs</a></li>
-									<li><a href="settings.php">General Settings</a></li>
-								</ul>
-						</li>
-						<li class="menu-list"><a href="#"><i class="fa fa-anchor"></i> <span>Manage Admin</span></a>  
-							<ul class="sub-menu-list">
-								<li><a href="add-admin.php">Add New</a> </li>
-								<li><a href="adminlist.php">Admin List</a> </li>
-							</ul>
-						</li>
-						<li><a href="logout.php"><i class="fa fa-sign-out"></i> <span>LogOut</span></a></li>	
-						</ul>
-				<!--sidebar nav end-->
-			</div>
-		</div>
-		<!-- left side end-->
-		<!-- main content start-->
-		<div class="main-content">
-			<!-- header-starts -->
-			<div class="header-section">
-			 
-			<!--toggle button start-->
-			<a class="toggle-btn  menu-collapsed"><i class="fa fa-bars"></i></a>
-			<!--toggle button end-->
-
-			<!--notification menu start -->
-			<div class="menu-right">
-				<div class="user-panel-top">  	
-					<div class="profile_details">		
-						<ul>
-							<li class="dropdown profile_details_drop">
-								<a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-expanded="false">
-									<div class="profile_img">	
-										 <div class="user-name">
-										 
-											<p><?php echo $_SESSION['SESS_FIRST_NAME']; ?><span>Administrator</span></p>
-										</div>
-										 <i class="lnr lnr-chevron-down"></i>
-										 <i class="lnr lnr-chevron-up"></i>
-										<div class="clearfix"></div>	
-									</div>	
-								</a>
-								<ul class="dropdown-menu drp-mnu">
-									<li> <a href="settings.php"><i class="fa fa-cog"></i> Settings</a> </li> 
-									<li> <a href="logout.php"><i class="fa fa-sign-out"></i> Logout</a> </li>
-								</ul>
-							</li>
-							<div class="clearfix"> </div>
-						</ul>
-					</div>		
-					       	
-					<div class="clearfix"></div>
-				</div>
-			  </div>
-			<!--notification menu end -->
-			</div>
-		<!-- //header-ends -->
+<!-- Basic Page Needs
+  ================================================== -->
+  <?php
+                           
+        $result = $db->prepare("SELECT * FROM settings");
+        $result->execute();
+        for($i=0; $row = $result->fetch(); $i++){
+                                    
+                  
+               ?> 
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"><title><?php echo $row['site_title'];?></title>
+  <meta name="keywords" content="<?php echo $row['site_keyword']; ?>" />
+  <meta name="description" content="<?php echo $row['site_discription']; ?>">
+  <meta name="author" content="">
+  <link rel="shortcut" href="images/favicon.html" />
+  <?php echo $row['google_code']; ?>
+  <?php } ?>
+<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0">
+<meta name="format-detection" content="telephone=no">
+<!-- CSS
+  ================================================== -->
+<link href="css/bootstrap.css" rel="stylesheet" type="text/css">
+<link href="plugins/mediaelement/mediaelementplayer.css" rel="stylesheet" type="text/css">
+<link href="css/style.css" rel="stylesheet" type="text/css">
+<link href="plugins/prettyphoto/css/prettyPhoto.css" rel="stylesheet" type="text/css">
+<!--[if lte IE 8]><link rel="stylesheet" type="text/css" href="css/ie8.css" media="screen" /><![endif]-->
+<!-- Color Style -->
+<link class="alt" href="colors/color1.css" rel="stylesheet" type="text/css">
+<link href="style-switcher/css/style-switcher.css" rel="stylesheet" type="text/css">
+<!-- SCRIPTS
+  ================================================== -->
+<script src="js/modernizr.js"></script><!-- Modernizr -->
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.6&appId=403202216515066";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
+</head>
+<body>
+<!--[if lt IE 7]>
+	<p class="chromeframe">You are using an outdated browser. <a href="http://browsehappy.com/">Upgrade your browser today</a> or <a href="http://www.google.com/chromeframe/?redirect=true">install Google Chrome Frame</a> to better experience this site.</p>
+<![endif]-->
+<div class="body"> 
+  <!-- Start Site Header -->
+  <header class="site-header">
+    <div class="topbar">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-4 col-sm-6 col-xs-8">
+            <h1 class="logo"> <a href="index.php"><img src="images/logo.png" alt="Logo"></a> </h1>
+          </div>
+          <div class="col-md-8 col-sm-6 col-xs-4">
+            <ul class="top-navigation ">
+            </ul>
+            <a href="#" class="visible-sm visible-xs menu-toggle"><i class="fa fa-bars"></i></a> </div>
+        </div>
+      </div>
+    </div>
+    <div class="main-menu-wrapper">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12">
+            <nav class="navigation">
+              <ul class="sf-menu">
+                <li><a href="index.php">Home</a>
+                 </li>
+                <li><a>About Us</a>
+				<ul class="dropdown">
+                                    <li><a href="about.php">About Us</a></li>
+                    <li><a href="our-excos.php">Executives</a></li>
+                  </ul>
+                </li>
+				                <li><a href="programmes.php">Programmes</a>
+                </li>
+                 <li><a href="events.php">Events</a>
+                  
+                </li>
+                <li><a href="news-updates.php">News Update</a>
+                </li>
+                <li><a href="gallery.php">Gallery</a>
+                </li>
+                <li><a href="contact.php">Contact Us</a> </li>
+				</ul>
+            </nav>
+          </div>
+        </div>
+      </div>
+    </div>
+  </header>
